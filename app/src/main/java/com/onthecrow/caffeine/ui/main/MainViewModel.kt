51f58d02fac7f005fc6957d584122c25d59af32b
onthecrow <onthecrow@doubletapp.io>
2024-08-25 17:16:30 +0100

    Separate UI state

--- app/src/main/java/com/onthecrow/caffeine/ui/main/MainViewModel.kt
+++ app/src/main/java/com/onthecrow/caffeine/ui/main/MainViewModel.kt
@@ -4,51 +4,54 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.onthecrow.caffeine.data.SettingsDataStore
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val settingsDataStore: SettingsDataStore
-): ViewModel() {
+) : ViewModel() {
 
-    private val isActive: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    private val _state: MutableStateFlow<MainState> =
+        MutableStateFlow(MainState(false, runBlocking { settingsDataStore.settings.first() }))
+    val state: StateFlow<MainState> get() = _state
 
-    // todo separate UI state and datastore, add debounce
-    val state: StateFlow<MainState> get() = combine(
-        isActive,
-        settingsDataStore.settings,
-    ) { isActive, settings ->
-        MainState(isActive, settings)
-    }.stateIn(viewModelScope, SharingStarted.Eagerly, MainState())
+    init {
+        @Suppress("OPT_IN_USAGE")
+        state.debounce(300)
+            .onEach { settingsDataStore.updateSettings(it.caffeineSettings) }
+            .flowOn(Dispatchers.IO)
+            .launchIn(viewModelScope)
+    }
 
     fun setIsPersistent(isPersistent: Boolean) {
-        viewModelScope.launch {
-            settingsDataStore.updateSettings(
-                settingsDataStore.settings.first().copy(isPersistent = isPersistent)
-            )
+        _state.update {
+            it.copy(caffeineSettings = it.caffeineSettings.copy(isPersistent = isPersistent))
         }
     }
 
     fun setIsRebootPersistent(isRebootPersistent: Boolean) {
-        viewModelScope.launch {
-            settingsDataStore.updateSettings(
-                settingsDataStore.settings.first().copy(isRebootPersistent = isRebootPersistent)
-            )
+        _state.update {
+            it.copy(caffeineSettings = it.caffeineSettings.copy(isRebootPersistent = isRebootPersistent))
         }
     }
 
     fun setIsAutomaticTurnOff(isAutomaticTurnOff: Boolean) {
-        viewModelScope.launch {
-            settingsDataStore.updateSettings(
-                settingsDataStore.settings.first().copy(isAutomaticallyTurnOff = isAutomaticTurnOff)
-            )
+        _state.update {
+            it.copy(caffeineSettings = it.caffeineSettings.copy(isAutomaticallyTurnOff = isAutomaticTurnOff))
         }
     }
 }
